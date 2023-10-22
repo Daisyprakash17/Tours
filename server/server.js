116e80f0237c7301819ebdeaea07161be7322558
Simone Capuano <scapuano19@gmail.com>
2023-10-22 14:16:38 +0200

    DB connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
-const app = require('./app');
+const mongoose = require('mongoose');
 require('dotenv').config();
+const app = require('./app');
+
+const DB_URL = process.env.MONGODB.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log('DB connection established');
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
