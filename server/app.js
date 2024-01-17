f1fb797aebfa538a3cdc1c609a48239136da8446
Simone Capuano <scapuano19@gmail.com>
2024-01-17 12:03:51 +0100

    Limit repeated requests to public APIs - limit repeated requests to public APIs using express-rate-limit

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./src/routes/tourRoutes');
 const userRouter = require('./src/routes/userRoutes');
 const errorHandler = require('./src/handlers/errorHandler');
 
 const app = express();
 
-// middlewares
+// global middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit repeated requests to public APIs
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 
