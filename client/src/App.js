d95d0c7ebf1e2f517b16c4036aa7c813ac39cea4
Simone Capuano <scapuano19@gmail.com>
2024-01-26 10:54:07 +0100

    footer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 
 const App = () => {
@@ -16,6 +17,7 @@ const AppWrapper = () => {
       <Router>
         <Header />
         <App />
+        <Footer />
       </Router>
     </>
   );
