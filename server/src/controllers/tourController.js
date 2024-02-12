b29ba73b1192c1a32ecdc8acd3259f784826edec
Simone Capuano <scapuano19@gmail.com>
2024-02-12 11:12:49 +0100

    upload tour images to the backend

--- server/src/controllers/tourController.js
+++ server/src/controllers/tourController.js
@@ -1,5 +1,64 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const factory = require('./handlerFactory');
+
+// Doc: https://www.npmjs.com/package/multer
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only images are allowed.'), false);
+  }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = async (req, res, next) => {
+  try {
+    // console.log(req.files)
+    if (!req.files.imageCover || !req.files.images) return next();
+
+    // Cover image
+    req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+    await sharp(req.files.imageCover[0].buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`../server/public/img/tours/${req.body.imageCover}`);
+
+    // Images
+    req.body.images = [];
+
+    await Promise.all(
+      req.files.images.map(async (file, index) => {
+        const fileName = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`;
+
+        await sharp(file.buffer)
+          .resize(2000, 1333)
+          .toFormat('jpeg')
+          .jpeg({ quality: 90 })
+          .toFile(`../server/public/img/tours/${fileName}`);
+
+        req.body.images.push(fileName);
+      })
+    );
+
+    next();
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -9,7 +68,10 @@ exports.aliasTopTours = (req, res, next) => {
 };
 
 exports.getAllTours = factory.getAll(Tour);
-exports.getTour = factory.getOne(Tour, [{ path: 'reviews' }, { path: 'guides'}]);
+exports.getTour = factory.getOne(Tour, [
+  { path: 'reviews' },
+  { path: 'guides' },
+]);
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -18,19 +18,26 @@ const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
-  if (!req.file) return next();
+exports.resizeUserPhoto = async (req, res, next) => {
+  try {
+    if (!req.file) return next();
 
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  // console.log(req.file)
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+    // console.log(req.file)
 
-  sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile(`../server/public/img/users/${req.file.filename}`);
+    await sharp(req.file.buffer)
+      .resize(500, 500)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`../server/public/img/users/${req.file.filename}`);
 
-  next();
+    next();
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
 const filterObj = (obj, ...allowedFields) => {
